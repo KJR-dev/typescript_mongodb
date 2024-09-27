2870bc94a0f2546532f1d042dce426cf6360e1cd
KJR-dev <sahooj168@gmail.com>
2024-09-28 04:12:03 +0530

    feat: Environment setup done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+// @ts-ignore
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config({
