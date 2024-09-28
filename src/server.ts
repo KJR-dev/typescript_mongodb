54b1eb2aa978281a9244616ac77c92366fcd4db6
KJR-dev <sahooj168@gmail.com>
2024-09-29 02:00:20 +0530

    feat: Express, error, response and gobal error handle

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,28 @@
+import app from './app'
+import config from './config/config'
 
+const server = app.listen(config.PORT)
 
-
+;(() => {
+    try {
+        //Database Conntion
+         // eslint-disable-next-line no-console
+        console.info(`APPLICATION_STARTD`, {
+            meta: {
+                PORT: config.PORT,
+                SERVER_URL: config.SERVER_URL
+            }
+        })
+    } catch (error) {
+         // eslint-disable-next-line no-console
+        console.log(`APPLICATION_ERROR`, { meta: error })
+        server.close((error) => {
+            if (error) {
+                 // eslint-disable-next-line no-console
+                console.log(`APPLICATION_ERROR`, { meta: error })
+            }
+            process.exit(1)
+        })
+    }
+})()
 
