00ca0b98bba76e08d4cd4ca05cf2cdef0e2423c1
KJR-dev <sahooj168@gmail.com>
2024-10-01 01:10:19 +0530

    feat: Helmet setup done

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import gobalErrorHandler from './middleware/gobalErrorHandler'
 import responseMessage from './constant/responseMessage'
 import httpError from './util/httpError'
 import { Request, Response } from 'express'
+import helmet from 'helmet'
 
 const app: Application = express()
 
 //Middleware
+app.use(helmet()) //Helmet
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
