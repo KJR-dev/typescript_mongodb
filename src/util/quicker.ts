5453ed007929f073da5b43e41c39642be01952f1
KJR-dev <sahooj168@gmail.com>
2024-10-01 01:05:06 +0530

    feat: Health api response modify

--- src/util/quicker.ts
+++ src/util/quicker.ts
@@ -5,8 +5,8 @@ export default {
     getSystemHealth: () => {
         return {
             cpuUsage: os.loadavg(),
-            totalmemory: `${os.totalmem() / 1024 / 1024} MB`,
-            freeMemory: `${os.freemem() / 1024 / 1024} MB`
+            totalmemory: `${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`,
+            freeMemory: `${(os.freemem() / 1024 / 1024).toFixed(2)} MB`
         }
     },
     getApplicationHealth: () => {
@@ -14,8 +14,8 @@ export default {
             environment: config.ENV,
             uptime: `${process.uptime().toFixed(2)} Second`,
             memoryUsage: {
-                heapTotal: `${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed()} MB`,
-                heapUsed: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed()} MB`
+                heapTotal: `${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} MB`,
+                heapUsed: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`
             }
         }
     }
